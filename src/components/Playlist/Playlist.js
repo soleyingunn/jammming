fff2bda75936503ee6e313d4f88996dbe769eafc
Lava <svanbergsdottir@gmail.com>
2023-08-08 18:00:29 +0000

    create initial components and css modules

---
+++ src/components/Playlist/Playlist.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Playlist() {
+  return (
+    <div className="Playlist">
+      <input defaultValue={"New Playlist"} />
+      {/* Add a TrackList component */}
+      <button className="Playlist-save">SAVE TO SPOTIFY</button>
+    </div>
+  );
+}
+
+export default Playlist;
